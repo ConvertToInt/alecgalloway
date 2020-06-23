e62704b47881524dee9b4cd077cb644333023ae8
ConvertToInt <tdalgleish7@gmail.com>
2020-06-23 15:21:58 +0100

    created a seperate js for portfolio page

--- alecgalloway/bulk.js
+++ alecgalloway/bulk.js
@@ -2,7 +2,7 @@
     $("#maintitle, #navs").css("opacity", 0 - $(window).scrollTop() * 100);
   }); */
 
- /*  $(window).scroll(function() {
+  $(window).scroll(function() {
     var scrollTop = $(this).scrollTop();
   
     $('.navbar-background-color').css({
@@ -39,44 +39,4 @@
     }
   
   });
-
-  document.querySelectorAll('.modal-button').forEach(function(el) {
-    el.addEventListener('click', function() {
-      var target = document.querySelector(el.getAttribute('data-target'));
-      
-      target.classList.add('is-active');
-      
-      target.querySelector('.modal-close').addEventListener('click',   function() {
-          target.classList.remove('is-active');
-       });
-    });
-  }); */
-
-
-  $(document).ready(function(){
-
-    $(".filter-button").click(function(){
-        var value = $(this).attr('data-filter');
-        
-        if(value == "all")
-        {
-            //$('.filter').removeClass('hidden');
-            $('.filter').show('1000');
-        }
-        else
-        {
-//            $('.filter[filter-item="'+value+'"]').removeClass('hidden');
-//            $(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
-            $(".filter").not('.'+value).hide('3000');
-            $('.filter').filter('.'+value).show('3000');
-            
-        }
-    });
-    
-    if ($(".filter-button").removeClass("active")) {
-$(this).removeClass("active");
-}
-$(this).addClass("active");
-
-});
 
---
+++ alecgalloway/portfolio.js
@@ -0,0 +1,39 @@
+document.querySelectorAll('.modal-button').forEach(function(el) {
+    el.addEventListener('click', function() {
+      var target = document.querySelector(el.getAttribute('data-target'));
+      
+      target.classList.add('is-active');
+      
+      target.querySelector('.modal-close').addEventListener('click',   function() {
+          target.classList.remove('is-active');
+       });
+    });
+  });
+
+
+  $(document).ready(function(){
+
+    $(".filter-button").click(function(){
+        var value = $(this).attr('data-filter');
+        
+        if(value == "all")
+        {
+            //$('.filter').removeClass('hidden');
+            $('.filter').show('1000');
+        }
+        else
+        {
+//            $('.filter[filter-item="'+value+'"]').removeClass('hidden');
+//            $(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
+            $(".filter").not('.'+value).hide('3000');
+            $('.filter').filter('.'+value).show('3000');
+            
+        }
+    });
+    
+    if ($(".filter-button").removeClass("active")) {
+$(this).removeClass("active");
+}
+$(this).addClass("active");
+
+});
