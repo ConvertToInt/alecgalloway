5333dc75a3d52530c92c7f570d85a08cf71f8785
ConvertToInt <tdalgleish7@gmail.com>
2020-06-24 17:15:52 +0100

    navbar complete

--- alecgalloway/bulk.js
+++ alecgalloway/bulk.js
@@ -2,41 +2,37 @@
     $("#maintitle, #navs").css("opacity", 0 - $(window).scrollTop() * 100);
   });*/
 
-  $(window).scroll(function() {
-    var scrollTop = $(this).scrollTop();
+  function checkScroll(){
+    var startY = $('.navbar').height() * 2; //The point where the navbar changes in px
   
-    $('.navbar-background-color').css({
-      opacity: function() {
-        var elementHeight = $(this).height();
-        return 1 - (elementHeight - scrollTop) / elementHeight;
-      }
-    });
-  });
-
-
-  document.addEventListener('DOMContentLoaded', () => {
-
-    // Get all "navbar-burger" elements
-    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
-  
-    // Check if there are any navbar burgers
-    if ($navbarBurgers.length > 0) {
-  
-      // Add a click event on each of them
-      $navbarBurgers.forEach( el => {
-        el.addEventListener('click', () => {
-  
-          // Get the target from the "data-target" attribute
-          const target = el.dataset.target;
-          const $target = document.getElementById(target);
-  
-          // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-          el.classList.toggle('is-active');
-          $target.classList.toggle('is-active');
-  
-        });
-      });
+    if($(window).scrollTop() > startY){
+        $('.navbar').addClass("scrolled");
+    }else{
+        $('.navbar').removeClass("scrolled");
     }
+  }
   
+  if($('.navbar').length > 0){
+    $(window).on("scroll load resize", function(){
+        checkScroll();
+    });
+  }
+
+  $(document).ready(function() {
+
+    const navbar = document.querySelector(".navbar");
+    var startY = $('.navbar').height() * 2
+
+    // Check for click events on the navbar burger icon
+    $(".navbar-burger").click(function() {
+  
+        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+        $(".navbar-burger").toggleClass("is-active");
+        $(".navbar-menu").toggleClass("is-active");
+
+        if(!($(window).scrollTop() > startY)){
+          $(".navbar").toggleClass("scrolled");
+        }
+    });
   });
 
--- alecgalloway/projects.js
+++ alecgalloway/projects.js
@@ -1,19 +1,3 @@
-function checkScroll(){
-  var startY = $('.navbar').height() * 2; //The point where the navbar changes in px
-
-  if($(window).scrollTop() > startY){
-      $('.navbar').addClass("scrolled");
-  }else{
-      $('.navbar').removeClass("scrolled");
-  }
-}
-
-if($('.navbar').length > 0){
-  $(window).on("scroll load resize", function(){
-      checkScroll();
-  });
-}
-
 /* var slideIndex = [1,1];
 var slideId = ["mySlides1", "mySlides2"]
 showSlides(1, 0);
