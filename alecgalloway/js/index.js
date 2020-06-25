bf1db458a1e63b2f265742a9f5d5eca43e119c41
ConvertToInt <tdalgleish7@gmail.com>
2020-06-25 10:01:27 +0100

    index main fade in/positioning/media queries. nav

---
+++ alecgalloway/js/index.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    var mq = window.matchMedia( "(max-width: 1024px)" );
+    if (mq.matches) {
+        $(".fade").hide(0).delay(75).fadeIn(2500);
+    }
+    else {
+        $(".fade").hide(0).delay(75).fadeIn(2500);
+        $(".fadeslower").hide(0).delay(300).fadeIn(2500);
+    }
+
+    
+});
